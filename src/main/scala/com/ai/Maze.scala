0b7a1a3e31559d8374d36231471d3a094c7c1dea
Zac Pustejovsky <zacp@spotify.com>
2016-07-09 13:04:38 -0400

    nonworking - restart with archetype

--- src/main/scala/com/ai/Maze.scala
+++ src/main/scala/com/ai/Maze.scala
@@ -8,39 +8,50 @@ package com.ai
 class Maze[T] {
   // TODO: fix type T -> is it Int?
 
-  def arrayColumnEdges(array2d: Array[Array[Char]]): collection.mutable.Map[Char, Char] = {
-    var outmap = collection.mutable.Map[Char, Char]
-    array2d
-      .iterator
-      .zipWithIndex
-      .map{
-        case (array, id) =>
-          id + array.size*id
-      }
-//      .map(_.iterator)
-
-
-//    >>> for i, row in enumerate(a):
-//    ...     print (i + (row.shape[0]-1)*i) + row
-//      ...
-//    [0 1 2]
-//    [3 4 5]
-//    [6 7 8]
-  }
-  def makeMazeGraph(maze: Array[Array[Char]]): Map[Int, Int] = {
+//  def arrayColumnEdges(array2d: Array[Array[Char]]): collection.mutable.Map[Char, Char] = {
+//    var outmap = collection.mutable.Map[Char, Char]
+//
+//    array2d
+//      .flatMap(
+//        entry =>
+//          entry
+//      )
+////      .iterator
+////      .zipWithIndex
+////      .map{
+////        case (array, rowId) =>
+////          val idvec = array.map(entry => (rowId + (array.length-1)*rowId) + entry)
+////          idvec.zip(array).map{
+////
+////          }
+//////          idvec
+////      }
+//////      .map(_.iterator)
+//
+//
+////    >>> for i, row in enumerate(a):
+////    ...     print (i + (row.shape[0]-1)*i) + row
+////      ...
+////    [0 1 2]
+////    [3 4 5]
+////    [6 7 8]
+//  }
+//  def makeMazeGraph(maze: Array[Array[Char]]): Map[Int, Int] = {
     // give each element of the array a unique id that becomes the index of the map
 
     // iterate through the rows to find edges
 
     // iterate through the columns to find edges
-  }
+//  }
 
   def main(): Unit = {
     val mazestring = scala.io.Source.fromFile("maze.txt")
       .getLines
       .toArray
-      .map(line => line.toArray)
-    mazestring
+      .foreach(println)
+//      .map(line => line.toArray)
+
+//    arrayColumnEdges(mazestring)
 
 //    var maze = new MazeGraph[Int]
 //    maze.graph = Map(1 -> List(2), 2-> List(1, 3), 3 -> List(2, 4, 5),
