4b4906d0605a72b376a6da846b338e3535d42e51
Zac Pustejovsky <zacp@spotify.com>
2016-07-15 01:01:19 -0400

    Functioning DFS search with backtracking; new unit tests for creation of state-space graph; all tests passing

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 scalacOptions ++= Seq("-target:jvm-1.8", "-deprecation", "-feature", "-unchecked")
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
 
 javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
 
--- src/main/scala/com/ai/search/datastructures/StateSapce.scala
+++ src/main/scala/com/ai/search/datastructures/StateSapce.scala
@@ -2,28 +2,45 @@ package com.ai.search.datastructures
 
 import scala.collection.mutable.ListBuffer
 
-/**
-  * Created by ZacP on 6/26/16.
-  */
 class SearchState[T](obj: T) {
   val data = obj
   var start: Boolean = false
   var goal: Boolean = false
+  var heuristic: Int = 0
   var children = ListBuffer[T]()
 
   def addChild(child: T): Unit = {
     children += child
   }
+  def setHeuristic(value: Int): Unit = {
+    heuristic = value
+  }
 }
 
 class StateSpaceGraph[T] {
   var nodes = collection.mutable.Map[T, SearchState[T]]()
+  private var start = new ListBuffer[T]
+  private var goal = new ListBuffer[T]
 
   def addNode(obj: T, newNode: SearchState[T]): Unit = {
     nodes += (obj -> newNode)
+    if (newNode.start.equals(true)) {
+      start += newNode.data
+    }
+    else if (newNode.goal.equals(true)) {
+      goal += newNode.data
+    }
   }
   def getNode(obj: T): SearchState[T] = {
     val gottenNode = nodes.getOrElse(obj, new SearchState[T](obj))
+    gottenNode
+  }
+  def getStartNode: SearchState[T] = {
+    val gottenNode = nodes.getOrElse(start.head, new SearchState[T](start.head))
+    gottenNode
+  }
+  def getGoalNode: SearchState[T] = {
+    val gottenNode = nodes.getOrElse(goal.head, new SearchState[T](goal.head))
     gottenNode
   }
 }
